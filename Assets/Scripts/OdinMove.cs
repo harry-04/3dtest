f75198ebd30255dce02abbe5373279002bf9ae66
harry-04 <72386526+harry-04@users.noreply.github.com>
2020-11-18 16:26:21 +0000

    playermove

--- Assets/Scripts/OdinMove.cs
+++ Assets/Scripts/OdinMove.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class OdinMove : MonoBehaviour
 {
-    
+    public Rigidbody rb;
+    public BoxCollider boxCollider;
+    public float moveSpeedX = 1f;
+    public float moveSpeedZ = 1f;
+    public float moveSpeedY = 1f;
 
 
 
     void Start()
     {
-        
+        rb = transform.GetComponent<Rigidbody>();
+        boxCollider = transform.GetComponent<BoxCollider>();
     }
 
     
@@ -19,6 +24,30 @@ public class OdinMove : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-moveSpeedX, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(+moveSpeedX, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, +moveSpeedZ);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -moveSpeedZ);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, +moveSpeedY, rb.velocity.z);
+        }
         
     }
 }
