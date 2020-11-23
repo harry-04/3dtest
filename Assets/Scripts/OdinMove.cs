e366716d2913fc2d10a71c3b4089df4a0a7ca5b4
harry-04 <72386526+harry-04@users.noreply.github.com>
2020-11-23 16:21:49 +0000

    animationsandflowers

--- Assets/Scripts/OdinMove.cs
+++ Assets/Scripts/OdinMove.cs
@@ -9,6 +9,7 @@ public class OdinMove : MonoBehaviour
     public float moveSpeedX = 1f;
     public float moveSpeedZ = 1f;
     public float moveSpeedY = 1f;
+    public Animator animator;
 
 
 
@@ -27,21 +28,41 @@ public class OdinMove : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector3(-moveSpeedX, rb.velocity.y, rb.velocity.z);
+            animator.SetBool("walk_a", true);
+        }
+        else
+        {
+            animator.SetBool("walk_a", false);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector3(+moveSpeedX, rb.velocity.y, rb.velocity.z);
+            animator.SetBool("walk_d", true);
+        }
+        else
+        {
+            animator.SetBool("walk_d", false);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, +moveSpeedZ);
+            animator.SetBool("walk_w", true);
+        }
+        else
+        {
+            animator.SetBool("walk_w", false);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -moveSpeedZ);
+            animator.SetBool("walk_s", true);
+        }
+        else
+        {
+            animator.SetBool("walk_s", false);
         }
 
         if (Input.GetKey(KeyCode.Space))
