e96be95a1dec44f421c7efee3b80773f746763df
harry-04 <72386526+harry-04@users.noreply.github.com>
2020-12-02 16:22:40 +0000

    treesandshooting

---
+++ Assets/Scripts/ChefShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefShoot : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform armEnd;
+    public Animator animator;
+  
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            animator.SetBool("shoot", true);
+        }
+        else
+        {
+            animator.SetBool("shoot", false);
+        }
+    }
+
+
+
+    public void Shoot()
+    {
+        Rigidbody projectileInstance;
+        projectileInstance = Instantiate(projectile, armEnd.position, armEnd.rotation) as Rigidbody;
+        projectileInstance.AddForce(-armEnd.forward * 350f);
+    }
+}
