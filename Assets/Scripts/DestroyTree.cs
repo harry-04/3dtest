b4c4c2d391548d71960978a4f040c18582dc9964
harry-04 <72386526+harry-04@users.noreply.github.com>
2020-12-06 23:13:17 +0000

    hewfewfe

---
+++ Assets/Scripts/DestroyTree.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTree : MonoBehaviour
+{
+    public Animator animator;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Projectiles")
+        {
+            animator.SetBool("isDestroyed", true);
+            Destroy(col.gameObject);
+        }
+    }
+}
